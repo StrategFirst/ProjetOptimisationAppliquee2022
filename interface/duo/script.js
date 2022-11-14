293f76148713c828a92b2f5b490f41920cc59c87
Mathieu T <mathieu.toulon@free.fr>
2022-11-14 16:24:13 +0100

    🖥️ Interface et échange avec serveur

---
+++ interface/duo/script.js
@@ -0,0 +1,85 @@
+
+document.getElementById('add').onclick = () => {
+	let tr = document.createElement('tr');
+	tr.innerHTML = `
+					<td><input type="text"/> </td>
+					<td><input type="number"/> </td>
+					<td><input type="number"/> </td>
+					<td><input type="text"/> </td>
+					<td><input type="text"/> </td>
+					<td><input type="text"/> </td>
+					<td><input type="number"/> </td>
+					<td><input type="number"/> </td>
+					<td><input type="number"/> </td>
+					<td><input type="number"/> </td>
+					<td><input type="number"/> </td>`
+	document.querySelector('#in tbody').appendChild(tr);
+};
+
+document.getElementById('remove').onclick = () => {
+	let cible = document.querySelector('#in tbody');
+	let rm = cible.lastElementChild;
+	if (rm) { cible.removeChild(rm) }
+};
+
+document.getElementById('submit').onclick = () => {
+	let listeDonnees = [...document.querySelector('#in tbody').children]
+		.map(tr => {
+			let tab = [...tr.querySelectorAll('input')].map(input => input.value);
+			tab[5] = tab[5].split`,`.map(x => x.trim())
+			return tab;
+		});
+
+	let result = { nbPersonnes: listeDonnees.length, nbHeuresHebdo: [], niveaux: [], nbHeuresMin: [], nbHeuresMax: [], niveauMin: [], niveauMax: [], nbStylesMin: [], instrusments: [], villes: [], styles: [] }
+
+	let names = []
+	for (let data of listeDonnees) {
+		let [Nom, NbHeuresHebdo, Niv, Instru, Ville, Styles, NbHMin, NbHMax, NivMin, NivMax, NbStylesMin] = data;
+
+		result.nbHeuresHebdo.push(NbHeuresHebdo);
+		result.niveaux.push(Niv);
+		result.nbHeuresMin.push(NbHMin);
+		result.nbHeuresMax.push(NbHMax);
+		result.niveauMin.push(NivMin);
+		result.niveauMax.push(NivMax);
+		result.nbStylesMin.push(NbStylesMin);
+		result.instrusments.push(Instru);
+		result.villes.push(Ville);
+		result.styles.push(Styles);
+
+		names.push(Nom);
+	}
+
+
+	let fichier =
+`
+nbPersonnes = ${result.nbPersonnes};
+
+nbHeuresHebdo = ${JSON.stringify(result.nbHeuresHebdo).replace(/"/g, '')};
+niveaux = ${JSON.stringify(result.niveaux).replace(/"/g, '')};
+
+nbHeuresMin = ${JSON.stringify(result.nbHeuresMin).replace(/"/g, '')};
+nbHeuresMax = ${JSON.stringify(result.nbHeuresMax).replace(/"/g, '')};
+niveauMin = ${JSON.stringify(result.niveauMin).replace(/"/g, '')};
+niveauMax = ${JSON.stringify(result.niveauMax).replace(/"/g, '')};
+nbStylesMin = ${JSON.stringify(result.nbStylesMin).replace(/"/g, '')};
+
+instrusments = ${JSON.stringify(result.instrusments).replace(/"/g, '')};
+villes = ${JSON.stringify(result.villes).replace(/"/g, '')};
+styles = [${result.styles.map(S => `{${S.join`,`}}`).join`,`}];
+`;
+
+	fetch('/api/duo', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({ 
+			data: fichier
+		})
+	})
+		.then( res => res.json() )
+		.then( liste => liste.pair.map( (a,b) => ([a-1,b]) ) )
+		.then( pair => pair.map( ([x,y]) => `<tr> <td>${names[x]}</td><td>${names[y]}</td> </tr>` ).join`` )
+		.then( html => document.getElementById('out').innerHTML = html )
+}
--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 import express from "express";
 import { exec } from "child_process";
 import { config as loadEnv } from "dotenv";
+import bodyParser from "body-parser";
+import { writeFileSync } from 'fs';
 
 loadEnv()
 
 const app = express()
 
+app.use( bodyParser.json() );
+
 app.get('/api' , (req,res) => {
 	exec( 'minizinc --output-mode json core/prog.mzn core/data.dzn' , ( err , stdout , stderr ) => {
+		if( err != null ) {
+			console.error( {err,stdout,stderr} );
+			return res.sendStatus(500);
+		}
+		res.send( JSON.parse( stdout.replace(/-(-+)/g,'') ) );
+	})
+})
+
+app.post('/api/duo' , (req,res) => {
+	let query = req.body;
+	let now = `query/data_${+(new Date())}.dzn`;
+
+	writeFileSync( now , query.data , {} )
+	exec( `minizinc --output-mode json core/prog.mzn ${now}` , ( err , stdout , stderr ) => {
 		if( err != null ) {
 			console.error( {err,stdout,stderr} );
 			return res.sendStatus(500);
